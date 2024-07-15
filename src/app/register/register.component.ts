8c86ccf6bcfff3d30a14e3ebf77fb06ac5490650
Lucas von Niederhäusern <lucaskvn@gmail.com>
2024-07-15 15:58:40 +0200

    changed map to key-value interface

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,32 +2,38 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+// TODO Move this to a separate file
+interface WordPair {
+  key: string,
+  value: string,
+}
+
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
-
 export class RegisterComponent {
 
   key: string = '';
   value: string = '';
   errorMessage: string = '';
 
-  wordPairs = new Map<string, string>([
-    ['house', 'Haus'],
-    ['book', 'Buch'],
-    ['tree', 'Baum'],
-    ['car', 'Auto']
-  ]);
+  wordPairs: WordPair[] = [
+    // ! Sample data
+    { key: 'Haus', value: 'house' },
+    { key: 'Baum', value: 'tree' },
+    { key: 'Wolke', value: 'cloud' },
+    { key: 'Stift', value: 'pen' }
+  ];
 
   addWordPair(): void {
     if (this.key.trim() === '' || this.value.trim() === '') {
       this.errorMessage = 'Both fields are required.';
     } else {
-      this.wordPairs.set(this.key, this.value);
+      this.wordPairs.push({ key: this.key, value: this.value });
       this.key = '';
       this.value = '';
       this.errorMessage = '';
