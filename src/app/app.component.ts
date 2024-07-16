48d628a2910bc8ca9e74521477fc6d05d97dbc06
Lucas von Niederhäusern <lucaskvn@gmail.com>
2024-07-16 12:08:32 +0200

    Created tab navigation element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
+
+interface ILink {
+  path: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterOutlet, MatTabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Wortschatz-Trainer';
+  links: ILink[] = [
+    { path: "register", label: "Register" },
+    { path: "study", label: "Study" },
+    { path: "exam", label: "Exam" },
+  ];
+  activePath = this.links[0].path;
 }
