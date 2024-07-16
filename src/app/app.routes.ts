96369c6fff0604fdd4e63ab1af94b0c744f8f1d3
Lucas von Niederhäusern <lucaskvn@gmail.com>
2024-07-16 12:08:02 +0200

    Updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
+import { StudyComponent } from './study/study.component';
+import { ExamComponent } from './exam/exam.component';
 
 export const routes: Routes = [
-    { path: '**', component: RegisterComponent, pathMatch: 'full' }
+    { path: '', redirectTo: 'register', pathMatch: 'full' },
+    { path: 'register', component: RegisterComponent },
+    { path: 'study', component: StudyComponent },
+    { path: 'exam', component: ExamComponent },
 ];
