672aea6232f348c858b1372ea8b0ac86f798ce89
Lucas von Niederhäusern <lucaskvn@gmail.com>
2024-07-22 12:07:29 +0200

    Basic implementation of study component

--- src/app/study/study.component.ts
+++ src/app/study/study.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WordpairService } from '../services/wordpair.service';
+import { WordPair } from '../models/WordPair';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-study',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './study.component.html',
   styleUrl: './study.component.css'
 })
-export class StudyComponent {
+export class StudyComponent implements OnInit {
+
+  currentWordPair: WordPair = { key: '', value: '' };
+  showKey: boolean = true;
+  answer: string = '';
+  correctAnswer: string = '';
+
+  constructor(private wordPairService: WordpairService) { }
+
+  ngOnInit(): void {
+    this.currentWordPair = this.wordPairService.getRandomWordPair();
+    this.showKey = Math.random() >= 0.5;
+  }
+
+  checkAnswer(): void {
+    let solution = (this.showKey ? this.currentWordPair.key : this.currentWordPair.value);
+    if (this.answer === solution) {
+      this.reset();
+    } else {
+      this.correctAnswer = solution;
+      setTimeout(() => {
+        this.reset();
+      }, 2000);
+    }
+  }
+
+  reset(): void {
+    this.answer = '';
+    this.correctAnswer = '';
+    this.nextWordPair();
+  }
+
+  nextWordPair(): void {
+    this.currentWordPair = this.wordPairService.getRandomWordPair();
+    this.showKey = Math.random() >= 0.5;
+  }
 
 }
