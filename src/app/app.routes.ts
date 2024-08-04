55b1036ffbfe911def45a40e332657d18c223314
Lucas von Niederhäusern <lucaskvn@gmail.com>
2024-08-04 15:52:58 +0200

    Added wildcard route matching

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,4 +7,5 @@ export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: 'study', component: StudyComponent },
     { path: 'exam', component: ExamComponent },
+    { path: '**', component: RegisterComponent, pathMatch: 'full' }
 ];
