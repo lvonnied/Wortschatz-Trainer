e2b66800bb37375db9bcf25ee16cb00d0d8c6cd3
Lucas von Niederhäusern <lucaskvn@gmail.com>
2024-08-04 16:38:38 +0200

    Implemented local storage usage

--- src/app/services/wordpair.service.ts
+++ src/app/services/wordpair.service.ts
@@ -7,10 +7,21 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class WordpairService {
-  private wordPairsSubject = new BehaviorSubject<WordPair[]>([]);
+  private localStorageKey = 'wordPairs';
+  private wordPairsSubject = new BehaviorSubject<WordPair[]>(this.getWordPairsFromLocalStorage());
   wordPairs$ = this.wordPairsSubject.asObservable();
 
   constructor(private randomWordService: RandomwordpairService) { }
+
+  private getWordPairsFromLocalStorage(): WordPair[] {
+    const data = localStorage.getItem(this.localStorageKey);
+    console.log('data', data);
+    return data ? JSON.parse(data) : [];
+  }
+
+  private saveWordPairsToLocalStorage(wordPairs: WordPair[]): void {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(wordPairs));
+  }
 
   getWordPairs(): WordPair[] {
     return this.wordPairsSubject.getValue();
@@ -25,26 +36,31 @@ export class WordpairService {
   addWordPair(key: string, value: string): void {
     const currentWordPairs = this.getWordPairs();
     this.wordPairsSubject.next([...currentWordPairs, { key, value }]);
+    this.saveWordPairsToLocalStorage(this.getWordPairs());
   }
 
   removeWordPair(index: number): void {
     const currentWordPairs = this.getWordPairs();
     currentWordPairs.splice(index, 1);
     this.wordPairsSubject.next([...currentWordPairs]);
+    this.saveWordPairsToLocalStorage(this.getWordPairs());
   }
 
   editWordPair(index: number, key: string, value: string): void {
     const currentWordPairs = this.getWordPairs();
     currentWordPairs[index] = { key, value };
     this.wordPairsSubject.next([...currentWordPairs]);
+    this.saveWordPairsToLocalStorage(this.getWordPairs());
   }
 
   deleteAllWordPairs(): void {
     this.wordPairsSubject.next([]);
+    localStorage.clear();
   }
 
   addSampleData(): void {
     const currentWordPairs = this.getWordPairs();
     this.wordPairsSubject.next([...currentWordPairs, this.randomWordService.getRandomWordPair()]);
+    this.saveWordPairsToLocalStorage(this.getWordPairs());
   }
 }
